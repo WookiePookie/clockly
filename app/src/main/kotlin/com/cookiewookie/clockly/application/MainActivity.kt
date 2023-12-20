c77c2ed69e838ce18fd01442e2a7c2de12d344d0
vadwwxz <vadwwxz@gmail.com>
2023-12-20 18:43:53 +0200

    set up xml project and remove .idea

--- app/src/main/kotlin/com/cookiewookie/clockly/application/MainActivity.kt
+++ app/src/main/kotlin/com/cookiewookie/clockly/application/MainActivity.kt
@@ -2,9 +2,13 @@ package com.cookiewookie.clockly.application
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.cookiewookie.clockly.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
 }
